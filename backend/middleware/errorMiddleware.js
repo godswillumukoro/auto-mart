8d2fc89ff5e58e8406342bb592f3c95f979330d3
Godswill Umukoro <umukoro6@gmail.com>
2022-05-18 09:31:09 +0100

    Car Routes && Controllers && Error Handling w/ express-async-handler

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
-// a middleware is a function that executes during the request/response cycle
+// a middleware is a function that executes during the request/response cycle. In our case, the errorHandler will override the default error handler passed by express.js
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
