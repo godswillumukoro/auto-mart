77222eb48e23e84441bc837ebbb1147f736bdb99
Godswill Umukoro <umukoro6@gmail.com>
2022-05-18 11:24:00 +0100

    Basic CRUD fucntionality implemented on the API

--- backend/controllers/carController.js
+++ backend/controllers/carController.js
@@ -6,7 +6,7 @@ const Car = require('../models/carModel');
 // @access Private
 const getCars = asyncHandler(async (req, res) => {
   const cars = await Car.find();
-  res.status(200).json({ cars });
+  res.status(200).json(cars);
 });
 
 // @desc Post car
@@ -14,26 +14,44 @@ const getCars = asyncHandler(async (req, res) => {
 // @access Private
 const postCar = asyncHandler(async (req, res) => {
   // setting the error handler
-  if (!req.body.name) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error('Add a text field');
   }
+  const car = await Car.create({
+    text: req.body.text,
+  });
   console.log(req.body.name);
-  res.status(200).json({ message: 'Set cars' });
+  res.status(200).json(car);
 });
 
 // @desc Update car
 // @route PUT /api/cars/:id
 // @access Private
 const updateCar = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update car ${req.params.id}` });
+  const car = await Car.findById(req.params.id);
+  if (!car) {
+    req.status(400);
+    throw new Error('Car not Found');
+  }
+  const updatedCar = await Car.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedCar);
 });
 
 // @desc Delete car
 // @route DELETE /api/cars/:id
 // @access Private
 const deleteCar = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete car ${req.params.id}` });
+  const car = await Car.findById(req.params.id);
+  if (!car) {
+    req.status(400);
+    throw new Error('Car not found');
+  }
+  const deletedCar = await Car.findByIdAndDelete(req.params.id, req.body);
+
+  res.status(200).json({ id: deletedCar.id });
 });
 
 module.exports = {
